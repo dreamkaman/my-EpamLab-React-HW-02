95b473b9b6f62aef9a2d9a079ff03d2e5f63ffcd
Dreamkaman <riabushko@gmail.com>
2023-01-06 01:15:41 +0200

    Added states for Registration component

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,16 +1,62 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 import Input from 'common/Input';
 import Button from 'common/Button';
-import { Link } from 'react-router-dom';
 
 import s from './Registration.module.css';
 
 const Registration = () => {
+	const [nameValue, setNameValue] = useState('');
+	const [emailValue, setEmailValue] = useState('');
+	const [passwordValue, setPasswordValue] = useState('');
+
+	const onChangeNameHandle = (e) => {
+		setNameValue(e.target.value);
+	};
+
+	const onChangeEmailHandle = (e) => {
+		setEmailValue(e.target.value);
+	};
+
+	const onChangePasswordHandle = (e) => {
+		setPasswordValue(e.target.value);
+	};
+
+	const onSubmitHandle = (e) => {
+		e.preventDefault();
+
+		const newUser = {
+			name: nameValue,
+			email: emailValue,
+			password: passwordValue,
+		};
+
+		console.log('Submit!');
+	};
 	return (
-		<form className={s.registrationForm}>
+		<form className={s.registrationForm} onSubmit={onSubmitHandle}>
 			<h2>Registration</h2>
-			<Input placeholder='Enter name' labelTxt='Name' width='350' />
-			<Input placeholder='Enter email' labelTxt='Email' />
-			<Input placeholder='Enter password' labelTxt='Password' />
+			<Input
+				placeholder='Enter name'
+				labelTxt='Name'
+				width='350'
+				value={nameValue}
+				onChange={onChangeNameHandle}
+			/>
+			<Input
+				placeholder='Enter email'
+				labelTxt='Email'
+				value={emailValue}
+				onChange={onChangeEmailHandle}
+			/>
+			<Input
+				placeholder='Enter password'
+				labelTxt='Password'
+				value={passwordValue}
+				type='password'
+				onChange={onChangePasswordHandle}
+			/>
 			<Button btnText='Registration' type='submit' />
 			<p className={s.warning}>
 				If you have an account you can{' '}
