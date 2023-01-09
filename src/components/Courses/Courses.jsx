dc814845a21a96e850b493ffd594d228acd06a62
Dreamkaman <riabushko@gmail.com>
2023-01-09 23:26:50 +0200

    Added corrections to App.js

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,28 +1,41 @@
+import { useContext } from 'react';
+
 import CourseCard from './components/CourseCard/CourseCard';
 import Button from 'common/Button';
 import SearchBar from './components/SearchBar';
+import { Context } from 'Context';
 
-import { getAuthors } from 'helpers/authorsString';
+import { convertAuthorsIdToNames } from 'helpers/authorsString';
 import { dateTransform } from 'helpers/dateGenerator';
 import { durationTransform } from 'helpers/pipeDuration';
 
 import s from './Courses.module.css';
 
-const Courses = ({ courses = [] }) => {
+const Courses = () => {
+	const context = useContext(Context);
+
+	const onAddNewCourseClick = () => {
+		context.setShowCourses(false);
+	};
+
 	return (
 		<section className={s.coursesSection}>
 			<div className={s.wrapper}>
-				<SearchBar />
-				<Button btnText='Add new course' />
+				<SearchBar value={context.filter} setFilter={context.setFilter} />
+				<Button btnText='Add new course' onClick={onAddNewCourseClick} />
 			</div>
 			<ul>
-				{courses.map((course) => {
+				{context.courses.map((course) => {
+					console.log(course);
 					return (
 						<CourseCard
 							key={course.id}
 							title={course.title}
 							description={course.description}
-							authors={getAuthors(course.authors)}
+							authorsName={convertAuthorsIdToNames(
+								course.authors,
+								context.authors
+							)}
 							duration={durationTransform(course.duration)}
 							created={dateTransform(course.creationDate)}
 						/>
